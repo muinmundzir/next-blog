36e781094b01ac793bbfa52035dc4f5db0d1be27
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2021-11-11 14:08:29 +0700

    add categorywidget to homepage

--- components/Categories.jsx
+++ components/Categories.jsx
@@ -1,5 +1,26 @@
+import { useState, useEffect } from 'react';
+import Link from 'next/link';
+import { getCategories } from '../services';
+
 const Categories = () => {
-  return <div>Categories</div>;
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    getCategories().then((newCategories) => setCategories(newCategories));
+  }, []);
+
+  return (
+    <div className="bg-white shadow-lg rounded-lg p-8 mb-8">
+      <h3 className="text-xl font-semibold border-b pb-4 mb-8 ">Categories</h3>
+      {categories.map((category) => (
+        <Link href={`/category/${category.slug}`} key={category.slug}>
+          <span className="cursor-pointer block pb-3 mb-3">
+            {category.name}
+          </span>
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default Categories;
