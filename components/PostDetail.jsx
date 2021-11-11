9a40ec98e165ca57cdedc181baba6c89da8c1449
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2021-11-11 16:15:05 +0700

    add post detail page

---
+++ components/PostDetail.jsx
@@ -0,0 +1,9 @@
+const PostDetail = () => {
+  return (
+    <div>
+      <h1>PostDetail</h1>
+    </div>
+  );
+};
+
+export default PostDetail;
--- services/index.js
+++ services/index.js
@@ -38,6 +38,41 @@ export const getPosts = async () => {
   return result.postsConnection.edges;
 };
 
+export const getPostDetails = async (slug) => {
+  const query = gql`
+    query GetPostDetail($slug: String!) {
+      post(where: { slug: $slug }) {
+        author {
+          bio
+          name
+          id
+          photo {
+            url
+          }
+        }
+        createdAt
+        slug
+        title
+        excerpt
+        featuredImage {
+          url
+        }
+        categories {
+          name
+          slug
+        }
+        content {
+          raw
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.post;
+};
+
 export const getRecentPosts = async () => {
   const query = gql`
   query GetPostDetails(){
@@ -56,7 +91,7 @@ export const getRecentPosts = async () => {
   return result.posts;
 };
 
-export const getSimilarPosts = async () => {
+export const getSimilarPosts = async (categories, slug) => {
   const query = gql`
     query GetPostDetails($slug: String!, $categories: [String!]) {
       posts(
@@ -76,7 +111,7 @@ export const getSimilarPosts = async () => {
     }
   `;
 
-  const result = await request(graphqlAPI, query);
+  const result = await request(graphqlAPI, query, { categories, slug });
 
   return result.posts;
 };
