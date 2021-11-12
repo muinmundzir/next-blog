2bf16e9dd3decfb9b7848d34cf5527b1a1533e3d
Muhammad Mu'in Mundzir <mundzirmuin@gmail.com>
2021-11-12 10:54:09 +0700

    add filter post by category page

--- services/index.js
+++ services/index.js
@@ -131,6 +131,43 @@ export const getCategories = async () => {
   return result.categories;
 };
 
+export const getCategoryPost = async (slug) => {
+  const query = gql`
+    query GetCategoryPost($slug: String!) {
+      postsConnection(where: { categories_some: { slug: $slug } }) {
+        edges {
+          cursor
+          node {
+            author {
+              bio
+              name
+              id
+              photo {
+                url
+              }
+            }
+            createdAt
+            slug
+            title
+            excerpt
+            featuredImage {
+              url
+            }
+            categories {
+              name
+              slug
+            }
+          }
+        }
+      }
+    }
+  `;
+
+  const result = await request(graphqlAPI, query, { slug });
+
+  return result.postsConnection.edges;
+};
+
 export const getComments = async (slug) => {
   const query = gql`
     query GetComments($slug: String!) {
